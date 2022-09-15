9319f92ff564cb5a7b95711acea2d11ebd00cd1c
NicoC0 <nicoconfaa@gmail.com>
2022-09-15 13:31:49 -0300

    changed exports names

--- pages/initiatives/index.vue
+++ pages/initiatives/index.vue
@@ -8,6 +8,6 @@
 
 <script>
 export default {
-  name: 'ContactPage'
+  name: 'InitiativePage'
 }
 </script>
--- pages/masterclass.vue
+++ pages/masterclass.vue
@@ -8,6 +8,6 @@
 
 <script>
 export default {
-  name: 'ContactPage'
+  name: 'MasterClassPage'
 }
 </script>
