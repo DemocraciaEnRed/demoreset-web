299247ef7c564ea79ffef5c28ac48b93050d8927
NicoC0 <nicoconfaa@gmail.com>
2022-09-19 17:54:02 -0300

    added english translates

--- locales/en/home.js
+++ locales/en/home.js
@@ -1,5 +1,29 @@
 const home = {
-  hello: 'Welcome to the home page!'
+  hello: "Learn about DemoReset's actions",
+  mainButtons: {
+    wannaBePart: 'I want to be part of DemoReset',
+    wannaResolve: 'I want to <b>solve problems in my region</b>',
+    wannaSearch: 'I am <b>looking for initiatives</b> to replicate',
+    wannaKnow: 'I want to <b>learn about barriers</b>'
+  },
+  bannerTitle: 'Deliberative Democracy in the Global South',
+  bannerDescription: 'About description',
+  bannerButton: 'Learn more',
+  hubsTitle: 'Choose a Hub and know the barriers it faces',
+  hubsButton: 'Join the network',
+  aboutDemoReset: 'About Demoreset',
+  aboutDemoResetText: 'In collaboration with the global office of the National Endowment for Democracy NED and the Open Policy Institute (Colombia), we brought together 100 organizations from the Global South, located especially in Sub-Saharan Africa, Eastern Europe, Southeast Asia and Latin America to restore our democracies.<br /> <br /> In this project, initiatives will be incorporated to participate in an experience that will <b>transform the panorama of democracy in the Global South</b>. From this space, you will be able to find tools and solutions from around the world to address the challenges that organizations like yours face in the process of using democratic innovation build alliances, spaces to develop and refine your proposals for change at a Global South Summit with specialized support , international mentoring, among other surprises. <br /> <br /> It is time to transform democracy from proposals and narratives from the Global South.',
+  whyBePart: 'Why be part',
+  whyBePartText: 'In collaboration with the global office of the National Endowment for Democracy NED and the Open Policy Institute (Colombia), we brought together 100 organizations from the Global South, located especially in Sub-Saharan Africa, Eastern Europe, Southeast Asia and Latin America to restore our democracies.<br /> <br /> In this project, initiatives will be incorporated to participate in an experience that will <b>transform the panorama of democracy in the Global South</b>. From this space, you will be able to find tools and solutions from around the world to address the challenges that organizations like yours face in the process of using democratic innovation build alliances, spaces to develop and refine your proposals for change at a Global South Summit with specialized support , international mentoring, among other surprises. <br /> <br /> <b>It is time to transform democracy from proposals and narratives from the Global South</b>.',
+  aboutButton: 'Learn more',
+  newsletterListTitle: 'Every month we are sending our on.reset where you will find three sections:',
+  newsletterList: {
+    item1: '1. How we are doing (findings and current project agenda)',
+    item2: "2. What's Coming Up (What are our upcoming events)",
+    item3: '3. Recommendations of the month (Three articles that will contribute to knowledge)'
+  },
+  proyectBy: 'A project of',
+  committee: 'Advisory Committee'
 }
 
 export default home
