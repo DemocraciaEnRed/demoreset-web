071e3682a6d6dfe41a86b8312da54dfef6139418
NicoC0 <nicoconfaa@gmail.com>
2022-09-22 21:41:43 -0300

    organization tab section

--- components/initiatives/OrganizationTab.vue
+++ components/initiatives/OrganizationTab.vue
@@ -1,5 +1,39 @@
 <template>
-  <p>OrganizationTab</p>
+  <section class="section">
+    <div class="columns mt-5">
+      <div class="column is-2">
+        <img :src="`${apiUrl}/assets/${initiative.organization.logo?.id}?width=120&height=120&fit=cover`" alt="">
+      </div>
+      <div class="column is-half">
+        <div>
+          <h3 class="is-inline mr-4 is-condensed is-uppercase">
+            {{ initiative.organization.name }}
+          </h3>
+          <span class="is-mono"><i class="fa-solid fa-location-dot" /> {{ initiative.country.name }}</span>
+        </div>
+        <div class="mt-5">
+          <p>Información de contacto</p>
+          <p v-if="initiative.organization.web" class="is-mono">
+            <span>WEB:</span> <a :href="`${initiative.organization.web}`" target="_blank" class="is-uppercase">{{ initiative.organization.web }}</a>
+          </p>
+          <p v-else class="is-mono is-uppercase">
+            WEB: No hay información disponible
+          </p>
+          <p v-if="initiative.organization.email" class="is-mono">
+            <span>EMAIL:</span> <a :href="`mailto:${initiative.organization.email}` " class="is-uppercase">{{ initiative.organization.email }}</a>
+          </p>
+          <p v-else class="is-mono is-uppercase">
+            EMAIL: No hay información disponible
+          </p>
+        </div>
+      </div>
+      <div class="column">
+        <button class="button is-rounded is-uppercase is-mono">
+          Perfil completo
+        </button>
+      </div>
+    </div>
+  </section>
 </template>
 <script>
 export default {
@@ -9,6 +43,18 @@ export default {
       type: Object,
       default: null
     }
+  },
+  computed: {
+    apiUrl () {
+      return process.env.API_URL
+    }
   }
 }
 </script>
+
+<style>
+  .button {
+    border: 2px solid #000;
+    font-weight: 500;
+  }
+</style>
