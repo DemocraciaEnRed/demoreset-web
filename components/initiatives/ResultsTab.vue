52be4eafb938ca4e7daa660dc0763902a2afb3e6
NicoC0 <nicoconfaa@gmail.com>
2022-09-23 08:46:26 -0300

    results section done

--- components/initiatives/ResultsTab.vue
+++ components/initiatives/ResultsTab.vue
@@ -1,5 +1,26 @@
 <template>
-  <p>ResultsTab</p>
+  <div class="container my-6">
+    <div class="block">
+      <div class="title is-condensed is-size-5 is-uppercase">
+        {{ $t('initiatives.resultsTab.goalsAccomplishment') }}
+      </div>
+      <div class="content is-uppercase is-condensed">
+        <ul>
+          <li>
+            {{ initiative.goal_accomplishment.name ? initiative.goal_accomplishment.name : $t('initiatives.noData') }}
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="block">
+      <div class="title is-condensed is-size-5 is-uppercase">
+        {{ $t('initiatives.resultsTab.initiativeResult') }}
+      </div>
+      <div class="block">
+        {{ initiative.key_results ? initiative.key_results : $t('initiatives.noData') }}
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
