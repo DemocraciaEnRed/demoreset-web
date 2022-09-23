7ac2073823f828310642fcdd2a8b5035d445b288
NicoC0 <nicoconfaa@gmail.com>
2022-09-23 11:15:25 -0300

    initial structure of the section

--- pages/organizations/_id.vue
+++ pages/organizations/_id.vue
@@ -1,13 +1,21 @@
 <template>
+  <!-- all section pending -->
   <section class="section">
     <div class="container">
-      This is the organization page
+      <div class="box">
+        <!-- organization card -->
+        <h2></h2>
+        <!-- info and interests table  -->
+        <OrganizationsTab />
+      </div>
     </div>
   </section>
 </template>
 
 <script>
+import OrganizationsTab from '../../components/organizations/organizationsTab.vue'
 export default {
-  name: 'OrganizationPage'
+  name: 'OrganizationPage',
+  components: OrganizationsTab
 }
 </script>
