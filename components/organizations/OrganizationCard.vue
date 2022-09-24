13dcb1061bfca95a4036d0b2850794f3269791ab
NicoC0 <nicoconfaa@gmail.com>
2022-09-24 15:29:52 -0300

    card containing the organization's initiatives

---
+++ components/organizations/OrganizationCard.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="card organization-card my-2">
+    <div class="card-content">
+      <div class="columns is-vcentered">
+        <div v-if="initiativeorg.organization.logo" class="column is-narrow">
+          <nuxt-link :to="{ path: localePath('/initiatives/' + initiativeorg.id) }">
+            <img :src="`${apiUrl}/assets/${initiativeorg.organization.logo?.id}?width=120&height=120&fit=cover`" alt="Logo" class="organization-logo">
+          </nuxt-link>
+        </div>
+        <div class="column">
+          <div class="has-text-left ml-4">
+            <p class="is-mono organization-name">
+              {{ initiativeorg.organization?.name }}
+            </p>
+            <!-- ANALIZAR CONSISTENCIA DE DATOS ORGANIZACION - CONDICIONAL NECESARIO? -->
+            <nuxt-link :to="{ path: localePath('/initiatives/' + initiativeorg.id) }" class="has-text-black">
+              <h4 class="is-mono is-uppercase initiative-title">
+                {{ initiativeorg.title }}
+              </h4>
+            </nuxt-link>
+          </div>
+        </div>
+        <div class="column is-3 initiative-date has-text-right-desktop has-text-centered-touch">
+          <div>
+            <p>{{ $t('initiatives.card.startDate') }}:</p>
+            <span>{{ initiativeorg.start_date }}</span>
+          </div>
+          <div>
+            <p>{{ $t('initiatives.card.status') }}</p>
+            <div>{{ initiativeorg.initiative_status == 'ongoing' ? $t('initiatives.ongoing') : $t('initiatives.finished') }}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OrganizationCard',
+  props: {
+    initiativeorg: {
+      type: Object,
+      default: null
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {
+    apiUrl () {
+      return process.env.API_URL
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.initiative-title {
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 27px;
+}
+
+.organization-name {
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 29px;
+}
+
+.organization-logo{
+  width: 120px;
+  height: auto;
+}
+
+.initiative-date {
+  p {
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 29px;
+    margin-bottom: 0;
+  }
+
+  span {
+    font-size: 16px;
+    line-height: 29px;
+  }
+}
+</style>
