ec5b0756170d935f7e794089abc53299d99cd20b
NicoC0 <nicoconfaa@gmail.com>
2022-09-24 15:31:48 -0300

    first translations added for the profile of organizations

--- locales/en/organizations.js
+++ locales/en/organizations.js
@@ -2,7 +2,9 @@ const organizations = {
   tableLabels: {
     information: 'Information',
     interests: 'Interests'
-  }
+  },
+  description: 'Description of the organization:',
+  initiatives: 'Initiatives'
 }
 
 export default organizations
--- locales/es/organizations.js
+++ locales/es/organizations.js
@@ -2,7 +2,9 @@ const organizations = {
   tableLabels: {
     information: 'Información',
     interests: 'Intereses'
-  }
+  },
+  description: 'Descripción de la oganización:',
+  initiatives: 'Iniciativas'
 }
 
 export default organizations
