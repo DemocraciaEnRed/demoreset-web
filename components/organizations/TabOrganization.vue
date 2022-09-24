015a7f9edc490d912aa62dd7c70cd1fafa4f6f41
NicoC0 <nicoconfaa@gmail.com>
2022-09-24 16:07:12 -0300

    added margin to separate the card from the title

--- components/organizations/TabOrganization.vue
+++ components/organizations/TabOrganization.vue
@@ -8,7 +8,7 @@
             <p>{{ $t('organizations.description') }}</p>
             <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit autem perferendis nulla, animi culpa dicta odio vel esse minus, atque ab illo magnam beatae ad similique aperiam repellat id? Facere. </p>
           </div>
-          <h4 class="is-mono is-uppercase has-text-weight-semibold">
+          <h4 class="is-mono is-uppercase has-text-weight-semibold mb-6">
             {{ $t('organizations.initiatives') }}
           </h4>
           <OrganizationCard :initiativeorg="initiativeorg" />
