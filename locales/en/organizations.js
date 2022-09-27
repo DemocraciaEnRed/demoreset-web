ab5715070350b19c600d96ead9c4b38a02a07be4
NicoC0 <nicoconfaa@gmail.com>
2022-09-27 17:09:21 -0300

    translations added to the organization profile

--- locales/en/organizations.js
+++ locales/en/organizations.js
@@ -1,10 +1,14 @@
 const organizations = {
+  noData: 'No information available.',
   tableLabels: {
     information: 'Information',
     interests: 'Interests'
   },
   description: 'Description of the organization:',
-  initiatives: 'Initiatives'
+  initiatives: 'Initiatives',
+  telephone: 'Telephone',
+  workTopics: 'Work topics',
+  barriersFaced: 'Barriers faced'
 }
 
 export default organizations
--- locales/es/organizations.js
+++ locales/es/organizations.js
@@ -1,10 +1,14 @@
 const organizations = {
+  noData: 'No hay información disponible.',
   tableLabels: {
     information: 'Información',
     interests: 'Intereses'
   },
   description: 'Descripción de la oganización:',
-  initiatives: 'Iniciativas'
+  initiatives: 'Iniciativas',
+  telephone: 'Teléfono',
+  workTopics: 'Temas de trabajo',
+  barriersFaced: 'Barreras que enfrenta'
 }
 
 export default organizations
