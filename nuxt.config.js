07d211040c7c1468b5ac10a1a7572b9281a6f400
Guillermo Croppi <guillermocroppi@gmail.com>
2022-09-28 21:52:50 -0300

    changed axios baseUrl

--- nuxt.config.js
+++ nuxt.config.js
@@ -68,7 +68,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseUrl: process.env.API_URL || 'https://demoreset.democraciaenred.org',
+    baseUrl: process.env.API_URL || 'https://content.demoreset.democraciaenred.org',
     prefix: '/graphql',
     credentials: true
   },
