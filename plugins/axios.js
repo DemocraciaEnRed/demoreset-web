4b5ff15a1f6a03802e43f387a497a5e49d4cb78e
NicoC0 <nicoconfaa@gmail.com>
2023-07-04 20:58:37 -0300

    remove token console.log

--- plugins/axios.js
+++ plugins/axios.js
@@ -7,10 +7,10 @@ export default function ({ $axios, store, app, $config }) {
     if (config.url.includes(`${$config.expressApi}`)) {
       // if there is in the state of the store a token, add it to the header of the request
       if (store.state.token) {
-        console.log('axios.js: token found in store')
+        // console.log('axios.js: token found in store')
         config.headers.common['x-access-token'] = store.state.token
       } else {
-        console.log('axios.js: no token found in store')
+        // console.log('axios.js: no token found in store')
       }
       return config
     } else {
